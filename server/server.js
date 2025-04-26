06bd7bdfe7bac4a3c3b6dc980513393646301a7d
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-04-26 19:03:34 +0530

    test

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ import cors from 'cors'
 import connectDB from './configs/mogoDb.js'
 const corsConfig = {
     origin:"*",
-    credential: true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-
 }
 //App Config
 const PORT = process.env.PORT || 4000;
