4f294546009414af20a7dea8cd9f479d60cc8d21
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-04-26 19:00:17 +0530

    TEST

--- server/server.js
+++ server/server.js
@@ -2,15 +2,20 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mogoDb.js'
+const corsConfig = {
+    origin:"*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 
+}
 //App Config
-const PORT = process.env.PORT || 4000
-const app = express()
-await connectDB()
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
 
 // Initialise Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors(corsConfig));
 
 // API Routes
 app.get('/',(req,res)=>res.send("API Working"))
