9dda9580feee45953adfba23cc8db6b9da2b252d
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-04-27 15:40:28 +0530

    Webhooks Added

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'
-dotenv.config();
-
+import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-// Middleware
-app.use(cors());
+// Initialise Middleware
 app.use(express.json());
+app.use(cors());
 
 // Connect DB
 await connectDB();
 
-// Routes
+// API Routes
 app.get('/', (req, res) => {
     res.send('API Working');
 });
+app.use('/api/user',userRouter)
 
 app.listen(PORT, () => {
   console.log(`Local server running on http://localhost:${PORT}`);
