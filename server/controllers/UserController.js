1871fc2813bb9804aa3c27d85b00d49b6324eb4b
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-04-27 16:42:09 +0530

    last name fix

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -36,7 +36,7 @@ const clerkWebhooks = async (req,res) =>{
                 const userData = {
                     email:data.email_addresses[0].email_address,
                     firstName:data.first_name,
-                    lastname:data.last_name,
+                    lastName:data.last_name,
                     photo:data.image_url
                 }
                 await userModel.findOneAndUpdate({clerkId:data.id},userData)
